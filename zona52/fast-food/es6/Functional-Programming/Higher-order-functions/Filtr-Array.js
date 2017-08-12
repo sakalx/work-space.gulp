0d23e3c7378358efe37371fcd9de35eb2f7ddea7
sakals <sakals@mail.ua>
2017-08-11 22:21:49 -0500

    new way filtr array

---
+++ zona52/fast-food/es6/Functional-Programming/Higher-order-functions/Filtr-Array.js
@@ -0,0 +1,44 @@
+'use strict';
+// filter array and creat new array
+const animals = [
+  {name: 'Fluki', species: 'dog'},
+  {name: 'Fluki', species: 'cat'},
+  {name: 'Fluki', species: 'rabbit'},
+  {name: 'Fluki', species: 'dog'},
+  {name: 'Fluki', species: 'fish'}
+];
+// better way
+
+const findDogs = unit => unit.species === 'dog';
+const dogs = animals.filter(findDogs);
+//const dogs = animals.filter(unit => unit.species === 'dog');
+console.log(dogs);
+
+//with using loop
+const dogsLoops = [];
+for (let unit in animals) {
+  if (animals[unit].species === 'dog')
+    dogsLoops.push(animals[unit]);
+}
+
+console.log(dogsLoops);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
