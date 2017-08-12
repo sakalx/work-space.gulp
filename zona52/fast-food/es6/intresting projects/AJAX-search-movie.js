4f3a3f06f89c4fc22c1f73d62dd406785ea8b114
sakals <sakals@mail.ua>
2017-08-11 22:26:49 -0500

    app movie searching

--- zona52/fast-food/es6/intresting projects/AJAX-search-movie.js
+++ zona52/fast-food/es6/intresting projects/AJAX-search-movie.js
@@ -1,25 +1,38 @@
-;(function() {
+;
+(function() {
   'use strict';
 
   //what movie you looking
-  const search = 'hitman',
-      //APi
-      url = `http://www.omdbapi.com/?apikey=57d13b99&s=${search}`,
-      //Node Movies list
+  const
+      search = document.getElementById('search'),
+      resultList = document.getElementById('foundList'),
       moviList = document.getElementById('movies');
 
 
-  //Render
+//Delete pre-elements
+  const removeEl = () => {
+    while (moviList.firstChild) {
+      moviList.removeChild(moviList.firstChild);
+    }
+    while (resultList.firstChild) {
+      resultList.removeChild(resultList.firstChild);
+    }
+  };
+
+  //Rendering
   // add HTML node with movie
   const renderHtml = movie => {
     const img = document.createElement('img');
     img.src = movie.Poster;
     moviList.appendChild(img);
+
+    const li = document.createElement('li');
+    li.innerHTML = `${movie.Title} <b>${movie.Year}</b>`;
+    resultList.appendChild(li);
   };
 
-  //add array movies for Rendering Html
-  const addMovies = movies => {
-    movies.Search.forEach(movie => renderHtml(movie));
+  const searchMovie = movies => {
+    movies.Search.forEach(movies => renderHtml(movies));
   };
   //End Render
 
@@ -34,18 +47,21 @@
   };
 
 //now make fetch
-  fetch(url).
-      then(checkStatus).
-      then(isJson => isJson.json()).
-      then(addMovies).
-      catch(err => console.error('x_x', err));
-  //end AJAX
+  const searching = (s) => {
+    if (s.target.value.length > 3) {
+      removeEl();
+      let url = `https://www.omdbapi.com/?apikey=57d13b99&s=${search.value}`;
+      fetch(url).
+          then(checkStatus).
+          then(isJson => isJson.json()).
+          then(searchMovie).
+          catch(err => console.error('x_x', err));
+    }
+  } ;
+
+  //Listener
+  search.addEventListener('keyup', searching);
 })();
-
-
-
-
-
 
 
 
