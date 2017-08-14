2a6bffef4a83da2890d8998a45332303440143eb
sakals <sakals@mail.ua>
2017-08-13 22:04:10 -0500

    change folder

--- src/js/slices/sender.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-(function () {
-  var sendMessage = function sendMessage() {
-    'use strict';
-  };
-
-  document.getElementById('send').addEventListener('click', sendMessage);
-})();
