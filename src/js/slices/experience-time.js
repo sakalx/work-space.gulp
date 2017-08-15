fb43323bc52d8de7d0b3e4cb91b667f1a4be9478
sakals <sakals@mail.ua>
2017-08-14 21:55:10 -0500

    change exp hrs /2

--- src/js/slices/experience-time.js
+++ src/js/slices/experience-time.js
@@ -8,7 +8,7 @@
 
   currentDate = new Date().getTime(),
       startDate = new Date(2017, 3, 1).getTime(),
-      passedHours = Math.round((currentDate - startDate) / hours);
+      passedHours = Math.round((currentDate - startDate) / hours / 2);
 
   document.getElementById('exp-hrs').innerHTML = ' ' + passedHours + '<sup>hrs</sup>';
 })();
