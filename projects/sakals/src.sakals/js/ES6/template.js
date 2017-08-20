1c393e23cd1afd11b7198381091ce020396187bb
sakals <sakals@mail.ua>
2017-08-19 22:10:46 -0500

    add hamburger Fn

---
+++ projects/sakals/src.sakals/js/ES6/template.js
@@ -0,0 +1,19 @@
+//name script
+/*
+ $(document).ready(function() {
+
+ });
+
+ ;(function() {
+ 'use strict';
+
+ })();
+
+ ;(() => {
+ 'use strict';
+
+ })();
+ */
+
+console.log(`%c 😈 Copyright © ${new Date().getFullYear()}`,
+`font-size:18px;color:#666`);
