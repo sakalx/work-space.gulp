4608fe441fe0d0864e2c91ecadd8c0d2aaea03fa
sakals <sakals@mail.ua>
2017-08-21 21:02:57 -0500

    learning React

---
+++ zona52/React/jsx/start.jsx
@@ -0,0 +1,25 @@
+const Hero = (props) => {
+  console.log(props);
+  return (
+      <div className="container">
+        <h1>{props.title}</h1>
+        <img src={props.imgUrl}/>
+        <p>{props.subTitle}</p>
+      </div>
+  );
+};
+
+
+
+ReactDOM.render(
+    <div>
+      <Hero
+          title="Kitty"
+          imgUrl="https://sakals.000webhostapp.com/share/HelloKitty.png"
+          subTitle="Best App! 2017"/>
+      <Hero
+          title="Kitty2"
+          imgUrl="https://sakals.000webhostapp.com/share/HelloKitty2.png"
+          subTitle="Best App! 2017"/>
+    </div>,
+    document.getElementById('root'));
