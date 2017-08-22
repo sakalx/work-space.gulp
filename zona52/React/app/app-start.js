6629154c0f0aef63aa02ea60a268c3de5cec124e
sakals <sakals@mail.ua>
2017-08-21 21:02:24 -0500

    trush

--- zona52/React/app/app-start.js
+++
@@ -1,6 +0,0 @@
-let img = React.createElement('img', { src: 'https://sakals.000webhostapp.com/share/HelloKitty.png' }),
-  title = React.createElement('h1', null, 'Hello Kitty'),
-  subTitlle = React.createElement('p',null, 'Best toy! 2017'),
-  conteiner = React.createElement('section', { className: 'wrap'}, title, img, subTitlle);
-
-ReactDOM.render(conteiner, document.getElementById('root'));
